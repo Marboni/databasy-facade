93f9c9f86d665c5aad620d614387fcc24dcb1f3a
Boris Marchenko <boma@ciklum.com>
2013-08-22 09:58:18 +0300

    Root test.

--- databasyfacade/databasyfacade/site/root/tests.py
+++ databasyfacade/databasyfacade/site/root/tests.py
@@ -1,8 +1,13 @@
+from flask import url_for
+import lxml.html
 from databasyfacade.testing import DatabasyTest
 
 __author__ = 'Marboni'
 
 class RootTest(DatabasyTest):
     def test_root(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
+        response = self.client.get(url_for('root.home'))
+        self.assert200(response)
+        html = lxml.html.fromstring(response.data)
+        login_links = html.xpath('//a[@href="%s"]' % url_for('auth.login'))
+        self.assertTrue(login_links)
